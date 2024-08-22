19a6f465c5b2603b6e4d9a3787005b8ce499b8ec
rifat3790 <mdrifayethossen@gmail.com>
2024-08-22 20:13:44 +0600

    load phone data from api

--- js/phone.js
+++ js/phone.js
@@ -1,7 +1,37 @@
-const loadPhone = async () =>{
-    const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
-    const data  = await res.json();
-    console.log(data.data);
-}
+const loadPhone = async () => {
+  const res = await fetch(
+    "https://openapi.programming-hero.com/api/phones?search=iphone"
+  );
+  const data = await res.json();
+  const phones = data.data;
+  // console.log(phones);
+  displayPhone(phones);
+};
+
+const displayPhone = (phones) => {
+    
+  const phoneContainer = document.getElementById("phone-container");
+
+  phones.forEach((phone) => {
+    console.log(phone);
+    const phoneCard = document.createElement("div");
+    phoneCard.classList = `card bg-gray-100 shadow-xl`;
+    phoneCard.innerHTML = `<figure class="px-10 pt-10">
+                      <img
+                        src="${phone.image}"
+                        alt="Shoes"
+                        class="rounded-xl" />
+                    </figure>
+                    <div class="card-body items-center text-center">
+                      <h2 class="card-title">${phone.phone_name}</h2>
+                      <p>${phone.slug}</p>
+                      <h2 class="card-title"> $999</h2>
+                      <div class="card-actions">
+                        <button class="btn btn-primary">Show Details</button>
+                      </div>
+                    </div>`;
+    phoneContainer.appendChild(phoneCard);
+  });
+};
 
 loadPhone();
